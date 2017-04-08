2af5c0855ec087c4a28415cd1153f32dddb005f5
맹도 <aw9223@naver.com>
2017-04-09 01:56:02 +0900

    commit

--- web/js/input/voiceCommand.js
+++ web/js/input/voiceCommand.js
@@ -1,20 +1,29 @@
 define([
     'jquery',
 ], function ($) {
-    
+
     var recog;
-    
-    var isStart;
-    
-    //https://ctrlq.org/code/19680-html5-web-speech-api
-  
+
     function init() {
         recog = new webkitSpeechRecognition();
         recog.continuous = true;
         recog.interimResults = true;
+        
+        recog.onresult = function (event) {
+            var str = '';
+            for (var n = event.resultIndex; n < event.results.length; ++n) {
+                if (event.results[n].isFinal)
+                    str += event.results[n][0].transcript;
+            }
+            
+            $('#homeActivity').html(str);
+        };
+
+        recog.start();
     }
-    
+
+
     return {
-        init : init
+        init: init
     }
 })
