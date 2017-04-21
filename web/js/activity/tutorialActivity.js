0bbbd33bc2e69503cd6a686748dcb2cb27e738e4
맹도 <aw9223@naver.com>
2017-04-21 10:35:20 +0900

    소슨정리, 드래그 학습추가

--- web/js/activity/tutorialActivity.js
+++ web/js/activity/tutorialActivity.js
@@ -1,34 +1,130 @@
-define([ 'system' ],function (system) {
+define([ 
+    'system', 
     
-    function reset() {
-        var box = $('#moveLearn > *:nth-child(3) > div');
-
-        var random = parseInt(Math.random() * box.length, 10);
+    'jquery',
+    'jquery-draggable'
+],function (system, $) {
+    
+    var __clickLearn__ = {
         
-        box.removeClass('target')
-           .eq(random)
-           .addClass('target');
+        init : function () {
+            var layoutLoaded = function () {
+                
+            }
+            
+            var learnWrapper = $('#tutorialActivity .learnWrapper');
+            learnWrapper.load('res/layout/activity_tutorial/click_learn.html', layoutLoaded);
+        },
+        
+        complete : function () {
+            
+        }
     }
+    
+    var __dragLearn__ = {
+        
+        successCounter : 0,
+        
+        init : function () {
+            var self = this;
+            
+            this.successCounter = 0;
+            
+            var layoutLoaded = function () {
+                $('#tutorialActivity .scrollWrapper')
+                    .on('dragend', function () { self.success() })
+                    .draggable({ 'axis' : 'x' });
+            }
+            
+            var learnWrapper = $('#tutorialActivity .learnWrapper');
+            learnWrapper.load('res/layout/activity_tutorial/drag_learn.html', layoutLoaded);
+        },
+        
+        success: function () {
+            this.successCounter++;
+
+            if (this.successCounter < 3) {
+                return;
+            }
+
+            __dragLearn__.complete();
+        },
+        
+        complete : function () {
+            __clickLearn__.init();
+        }
+    }
+    
+    var __moveLearn__ = {
+        
+        successCounter : 0,
+        
+        init : function () {
+            var self = this;
+            
+            this.successCounter = 0;
+            
+            var learnWrapper = $('#tutorialActivity .learnWrapper');
+            
+            $(learnWrapper)
+                .on(
+                    'mouseover mousemove', 
+                    '.targetObject', 
+                    function () {
+                        self.success();
+                    }
+                );
+            
+            var layoutLoaded = function () {
+                self.reset();
+            }
+            
+            learnWrapper.load('res/layout/activity_tutorial/move_learn.html', layoutLoaded);
+        },
+        
+        success: function () {
+            this.successCounter++;
+
+            if (this.successCounter < 3) {
+                this.reset();
+                return;
+            }
+
+            __moveLearn__.complete();
+        },
+        
+        reset : function () {            
+            var boxObjects = $('#tutorialActivity .learnStage div');
+            
+            var randomIndex = parseInt(Math.random() * boxObjects.length, 10);
+            
+            boxObjects
+                .removeClass('targetObject')
+                .eq(randomIndex)
+                .addClass('targetObject');
+        },
+        
+        complete : function () {
+            __dragLearn__.init();
+        }
+    }
+    
     
     return {
         
         id : 'tutorialActivity',
-        title : '튜토리얼',
+        title : '모션학습',
         icon : 'ic_tutorial.png',
         layoutHTML : 'activity_tutorial.html',
         
         init : function () {
             console.log('tutorial init');
-            
-            $(document).on('mouseover mousemove', '#moveLearn .target', function () {
-                reset();
-            })
         },
         
         resume : function () {
-            console.log('tutorial resume');
+            console.log('tutorial resume'); 
             
-            reset();
+            __moveLearn__.init();
         },
         
         pause : function () {
