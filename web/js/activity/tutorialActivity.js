9defeaa3c706c27b61193626d9104993d0babe02
맹도 <aw9223@naver.com>
2017-04-24 23:22:25 +0900

    파일분할

--- web/js/activity/tutorialActivity.js
+++ web/js/activity/tutorialActivity.js
@@ -2,135 +2,10 @@ define([
     'system', 
     
     'jquery',
-    'jquery-draggable'
-],function (system, $) {
+    'jquery-draggable',
     
-    var __clickLearn__ = {
-        
-        successCounter : 0,
-        
-        init : function () {
-            var self = this;
-        
-            this.successCounter = 0;
-            
-            var layoutLoaded = function () {
-                $('#tutorialActivity .learnStage div')
-                    .click(function () { self.success(this) })
-            }
-            
-            var learnWrapper = $('#tutorialActivity .learnWrapper');
-            learnWrapper.load('res/layout/activity_tutorial/click_learn.html', layoutLoaded);
-        },
-        
-        success: function (targetObject) {
-            targetObject = $(targetObject);
-            
-            if (targetObject.css('visibility') === 'hidden') {
-                return;
-            }
-            
-            targetObject.css('visibility', 'hidden');
-
-            if (++this.successCounter < 3) {
-                return;
-            }
-
-            __clickLearn__.complete();
-        },
-        
-        complete : function () {
-            
-        }
-    }
-    
-    var __dragLearn__ = {
-        
-        successCounter : 0,
-        
-        init : function () {
-            var self = this;
-            
-            this.successCounter = 0;
-            
-            var layoutLoaded = function () {
-                $('#tutorialActivity .scrollWrapper')
-                    .on('dragend', function () { self.success() })
-                    .draggable({ 'axis' : 'x' });
-            }
-            
-            var learnWrapper = $('#tutorialActivity .learnWrapper');
-            learnWrapper.load('res/layout/activity_tutorial/drag_learn.html', layoutLoaded);
-        },
-        
-        success: function () {
-            this.successCounter++;
-
-            if (this.successCounter < 3) {
-                return;
-            }
-
-            __dragLearn__.complete();
-        },
-        
-        complete : function () {
-            __clickLearn__.init();
-        }
-    }
-    
-    var __moveLearn__ = {
-        
-        successCounter : 0,
-        
-        init : function () {
-            var self = this;
-            
-            this.successCounter = 0;
-            
-            var learnWrapper = $('#tutorialActivity .learnWrapper');
-            
-            $(learnWrapper)
-                .on(
-                    'mouseover mousemove', 
-                    '.targetObject', 
-                    function () {
-                        self.success();
-                    }
-                );
-            
-            var layoutLoaded = function () {
-                self.reset();
-            }
-            
-            learnWrapper.load('res/layout/activity_tutorial/move_learn.html', layoutLoaded);
-        },
-        
-        success: function () {
-            this.successCounter++;
-
-            if (this.successCounter < 3) {
-                this.reset();
-                return;
-            }
-
-            __moveLearn__.complete();
-        },
-        
-        reset : function () {            
-            var targetObjects = $('#tutorialActivity .learnStage div');
-            var targetRandom = parseInt(Math.random() * targetObjects.length, 10);
-            
-            targetObjects
-                .removeClass('targetObject')
-                .eq(targetRandom)
-                .addClass('targetObject');
-        },
-        
-        complete : function () {
-            __dragLearn__.init();
-        }
-    }
-    
+    'activity/tutorialActivity/moveLearn'
+],function (system, $, moveLearn) {
     
     return {
         
@@ -146,7 +21,7 @@ define([
         resume : function () {
             console.log('tutorial resume'); 
             
-            __moveLearn__.init();
+            moveLearn.init();
         },
         
         pause : function () {
---
+++ web/js/activity/tutorialActivity/clickLearn.js
@@ -0,0 +1,41 @@
+define([
+    'jquery'
+], function ($) {
+    
+    var successCount = 0;
+    
+    function init() {
+        successCount = 0;
+
+        var layoutLoaded = function () {
+            $('#tutorialActivity .learnStage div')
+                .click(function () { success(this) })
+        }
+
+        var learnWrapper = $('#tutorialActivity .learnWrapper');
+        learnWrapper.load('res/layout/activity_tutorial/click_learn.html', layoutLoaded);
+    }
+
+    function success(targetObject) {
+        targetObject = $(targetObject);
+
+        if (targetObject.css('visibility') === 'hidden') {
+            return;
+        }
+
+        targetObject.css('visibility', 'hidden');
+
+        if (++successCount < 3) {
+            return;
+        }
+
+        complete();
+    }
+ 
+    function complete() {
+    }
+    
+    return {
+        init : init
+    }
+})
