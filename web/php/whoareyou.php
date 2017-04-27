0bd045be86332a5825c1c226bd7f06cb56021f6a
dkrdj8989 <dkrdj8989@naver.com>
2017-04-28 04:09:53 +0900

    달력
    htdocs에 GoogleCalendarApi-master가 있어야 정상작동

---
+++ web/php/whoareyou.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <form action="http://localhost/GoogleCalendarApi-master/public/api/oauth" method="get">
+      <input type="text" name="name" placeholder="누구세여">
+      <input type="hidden" name="done" value="a">
+      <input type="submit" value="띵동">
+    </form>
+  </body>
+</html>
