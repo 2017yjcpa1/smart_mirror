37915d6bbef93f30dd3745d93fc6523ab24c384d
jihohan1312 <hanho1312@naver.com>
2017-05-01 14:02:12 +0900

    지도 레이아웃 미세조정

--- web/js/activity/mapsActivity.js
+++ web/js/activity/mapsActivity.js
@@ -1,38 +1,38 @@
 define(['system', 'jquery'], function (system, $) {
-            var directionsDisplay = new google.maps.DirectionsRenderer();
-            var map;
+    var directionsDisplay = new google.maps.DirectionsRenderer();
+    var map;
 
-            function initialize() {
-                var youngjin = new google.maps.LatLng(35.896205, 128.622019);
-                var mapOptions = {
-                    zoom: 17,
-                    mapTypeId: google.maps.MapTypeId.ROADMAP,
-                    center: youngjin
-                };
-                map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-                directionsDisplay.setMap(map);
+    function initialize() {
+        var youngjin = new google.maps.LatLng(35.896205, 128.622019);
+        var mapOptions = {
+            zoom: 17,
+            mapTypeId: google.maps.MapTypeId.ROADMAP,
+            center: youngjin
+        };
+        map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+        directionsDisplay.setMap(map);
+    }
+
+    function calcRoute() {
+        console.log(arguments.callee);
+        var start = document.getElementById('start').value;
+        var end = document.getElementById('end').value;
+
+        var request = {
+            origin: start,
+            destination: end,
+            travelMode: google.maps.DirectionsTravelMode.TRANSIT
+        };
+        var directionsService = new google.maps.DirectionsService();
+        directionsService.route(request, function (response, status) {
+            console.log(response, status);
+            if (status === google.maps.DirectionsStatus.OK) {
+                directionsDisplay.setDirections(response);
             }
+        });
+    }
 
-            function calcRoute() {
-                console.log(arguments.callee)
-                var start = document.getElementById('start').value;
-                var end = document.getElementById('end').value;
 
-                var request = {
-                    origin: start,
-                    destination: end,
-                    travelMode: google.maps.DirectionsTravelMode.TRANSIT
-                };
-                var directionsService = new google.maps.DirectionsService();
-                directionsService.route(request, function (response, status) {
-                    console.log(response, status)
-                    if (status === google.maps.DirectionsStatus.OK) {
-                        directionsDisplay.setDirections(response);
-                    }
-                });
-            }
-
-            
 
     return {
         id: 'mapsActivity',
@@ -43,10 +43,10 @@ define(['system', 'jquery'], function (system, $) {
             console.log('maps init');
         },
         resume: function () {
-            console.log('maps resume'); 
+            console.log('maps resume');
             initialize();
             $('#mapsearch').click(calcRoute);
-            
+
         },
         pause: function () {
             console.log('maps pause');
