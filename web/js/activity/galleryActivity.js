63e8613c6d92001f89e762a5b7fcafba389c136f
00125 <00125_@naver.com>
2017-05-11 18:28:52 +0900

    갤러리 소심한 업데이트

--- web/js/activity/galleryActivity.js
+++ web/js/activity/galleryActivity.js
@@ -1,38 +1,44 @@
-define([ 'system','jquery','jquery-draggable' ],function (system) {
-    
+define(['system', 'jquery', 'jquery-draggable'], function (system) {
+
     return {
-        
-        id : 'galleryActivity',
-        title : '갤러리',
-        icon : 'ic_gallery.png',
-        layoutHTML : 'activity_gallery.html',
-        
-        init : function () {
+
+        id: 'galleryActivity',
+        title: '갤러리',
+        icon: 'ic_gallery.png',
+        layoutHTML: 'activity_gallery.html',
+
+        init: function () {
             console.log('gallery init');
         },
-        
-        resume : function () {
+
+        resume: function () {
             console.log('gallery resume');
-              $(document).ready(function () {
-                var path='/smart_mirror/gallery/';
-                var i=0;
+
+            $(document).ready(function () {
+                var path = '/smart_mirror/gallery/';
+                var i = 0;
+                
                 $.ajax({
-                    url:'/smart_mirror/web/php/pic_load_process.php',
-                    success: function(data){
-                        var file=JSON.parse(data);
-                        for(i; i<file.length; i++)
-                        $('#gallery').append("<img class='pics' src='"+path+file[i]+"'>")
+                    url: '/smart_mirror/web/php/pic_load_process.php',
+                    success: function (data) {
+                        var file = JSON.parse(data);
+                        for (i; i < file.length; i++) {
+                            $('#gallery').append("<a href='" + path + file[i] + "'><img class='sisi' src='" + path + file[i] + "'></a>")
+                        }
                     }
-                })           
+                })
             })
+
+            
+            
         },
-        
-        pause : function () {
+
+        pause: function () {
             console.log('gallery pause');
             $('#gallery').empty();
         },
-        
-        destroy : function () {
+
+        destroy: function () {
             console.log('gallery destroy');
         },
     }
