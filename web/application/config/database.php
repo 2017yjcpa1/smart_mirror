3ad6dc21f826b7c8f233b0f135b6d2b6ffc5e66e
maeng-do <fukeric@gmail.com>
2017-07-07 19:46:32 +0900

    db

--- web/application/config/database.php
+++ web/application/config/database.php
@@ -49,10 +49,10 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
-$db['default']['dbdriver'] = 'mysql';
+$db['default']['username'] = '3cpa1';
+$db['default']['password'] = 'sksmsqudtls!@#';
+$db['default']['database'] = '3cpa1';
+$db['default']['dbdriver'] = 'mysqli';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
 $db['default']['db_debug'] = TRUE;
