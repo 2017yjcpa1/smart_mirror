1c15876b9d53991ee07a7bf210abf08d6100961a
maeng-do <fukeric@gmail.com>
2017-07-08 17:10:17 +0900

    commit

--- web/application/views/welcome.php
+++ web/application/views/welcome.php
@@ -31,7 +31,7 @@
                 z-index: 0;
                 
                 width: 100%;
-                height: 100%;
+/*                height: 100%;*/
                 
                 background: black;
                 visibility: hidden;
