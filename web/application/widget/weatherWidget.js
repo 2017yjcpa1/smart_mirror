8e3c8945779c4ddd1c63661b969bfaed4724988c
maeng-do <fukeric@gmail.com>
2017-09-03 22:17:57 +0900

    gps 기반으로 날씨정보 가져오기

--- web/application/widget/weatherWidget.js
+++ web/application/widget/weatherWidget.js
@@ -7,22 +7,27 @@ define([
     var DEBUG_LAT = '35.8491';
     var DEBUG_LON = '128.5341';
     
+    function loadedGPS(gps) {
+        
+        darksky(
+            gps.coords.latitude, 
+            gps.coords.longitude, 
+            function (data) {                    
+                $('#weatherWidget img').attr('src', data.currently.iconPath);
+
+                $('#weatherWidget .temp').text(data.currently.temp + '℃');
+                $('#weatherWidget .precipProbabl').text('강수확률' + data.currently.precipProbabl + '%');
+            }
+        )
+    }
+    
     return {
         
         layoutHTML: 'widget_weather.html',
         
         init: function () {
             
-            darksky(
-                DEBUG_LAT, 
-                DEBUG_LON, 
-                function (data) {                    
-                    $('#weatherWidget img').attr('src', data.currently.iconPath);
-                    
-                    $('#weatherWidget .temp').text(data.currently.temp + '℃');
-                    $('#weatherWidget .precipProbabl').text('강수확률' + data.currently.precipProbabl + '%');
-                }
-            )
+            navigator.geolocation.getCurrentPosition(loadedGPS);
         },
     }
 })
