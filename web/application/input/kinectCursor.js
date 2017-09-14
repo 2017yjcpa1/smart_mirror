3ebed62bf75e348fa87a93f1c75a173905c127c3
maeng-do <fukeric@gmail.com>
2017-09-14 13:47:38 +0900

    commit

--- web/application/input/kinectCursor.js
+++ web/application/input/kinectCursor.js
@@ -274,7 +274,19 @@ define([
     function update(data) { 
         updateImage(data);
         
+        /*
         if (data.handRight.y < data.elbowRight.y) {
+            deactivate();
+            return false;
+        }
+        
+        if (data.handRight.y < data.hipRight.y) {
+            deactivate();
+            return false;
+        }
+        */
+        
+        if (data.handRight.y < data.spineMid.y) {
             deactivate();
             return false;
         }
